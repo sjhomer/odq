480bed918c7f29dbda50c9eef5522be570aaeae1
SJ Homer <76261430+sjhomer@users.noreply.github.com>
2023-06-14 19:58:07 -0400

    Update install.sh
    Disable plist brew updated

--- home/bin/brew/install.sh
+++ home/bin/brew/install.sh
@@ -10,20 +10,20 @@ else
 fi
 
 # Initialize brew auto-update on system load
-echo "Initializing brew auto-update on system load..."
-load_plist="$CWD"/home/bin/load_plist
-$load_plist "$HOME"/bin/brew/com.localBrew.autoUpdateOnStartup.plist
+#echo "Initializing brew auto-update on system load..."
+#load_plist="$CWD"/home/bin/load_plist
+#$load_plist "$HOME"/bin/brew/com.localBrew.autoUpdateOnStartup.plist
 
 # Check if suderos doesn't exist
-if [ ! -f /etc/sudoers.d/suderos ]; then
-  echo "Creating suderos..."
-  sudo touch /etc/sudoers.d/suderos
-fi
+#if [ ! -f /etc/sudoers.d/suderos ]; then
+#  echo "Creating suderos..."
+#  sudo touch /etc/sudoers.d/suderos
+#fi
 
 # Check if suderos doesn't include the autoUpdateOnStartup.sh script
-if ! cat /etc/sudoers.d/suderos | grep -qF "autoUpdateOnStartup.sh"; then
-  echo "Adding brew auto-update to suderos..."
-  echo "suderos ALL=(ALL) NOPASSWD: /bin/bash -c $HOME/brew/autoUpdateOnStartup.sh" | sudo tee -a /etc/sudoers.d/suderos
-else
-  echo "Brew auto-update already in suderos..."
-fi
+#if ! cat /etc/sudoers.d/suderos | grep -qF "autoUpdateOnStartup.sh"; then
+#  echo "Adding brew auto-update to suderos..."
+#  echo "suderos ALL=(ALL) NOPASSWD: /bin/bash -c $HOME/brew/autoUpdateOnStartup.sh" | sudo tee -a /etc/sudoers.d/suderos
+#else
+#  echo "Brew auto-update already in suderos..."
+#fi
