09b862329fb60f07dc29c769626254327e6f7d1c
SJ Homer <sean.homer@battlefy.com>
2023-06-13 22:39:19 -0400

    fix: ensuring odq main process is a function that can edit with a return vs error for skipping install

--- odq.sh
+++ odq.sh
@@ -3,6 +3,7 @@
 # Export the current directory
 CWD=$(pwd)
 
+function odq() {
 # https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Blocks&text=Odq
 echo " .----------------.  .----------------.  .----------------.
 | .--------------. || .--------------. || .--------------. |
@@ -16,41 +17,44 @@ echo " .----------------.  .----------------.  .----------------.
 | '--------------' || '--------------' || '--------------' |
  '----------------'  '----------------'  '----------------' "
 
-printf "\nOSx Developer Quickstart by SJ Homer - https://github.com/sjhomer/odq\n
-NOTE: Sudo access will be required for some steps.\n
-Begin OSx Development Quickstart? "
+  printf "\nOSx Developer Quickstart by SJ Homer - https://github.com/sjhomer/odq\n
+  NOTE: Sudo access will be required for some steps.\n
+  Begin OSx Development Quickstart? "
 
-# Prompt user to begin or exit
-echo "Enter 1 to begin, or 2 to review/adjust the config.sh first."
-select yn in "Continue" "Exit"; do
-  case $yn in
-    Continue ) break;;
-    Exit ) echo -n "No worries! Take your time and adjust the \`config.sh\` before re-running \`. $CWD/odq.sh\`" && return 0;;
-  esac
-done
+  # Prompt user to begin or exit
+  echo "Enter 1 to begin, or 2 to review/adjust the config.sh first."
+  select yn in "Continue" "Exit"; do
+    case $yn in
+      Continue ) break;;
+      Exit ) echo -n "No worries! Take your time and adjust the \`config.sh\` before re-running \`. $CWD/odq.sh\`" && return 0;;
+    esac
+  done
 
-source "$CWD"/config.sh
-echo "Config loaded"
+  source "$CWD"/config.sh
+  echo "Config loaded"
 
-# Install bin scripts & Homebrew
-source "$CWD"/home/install.sh
+  # Install bin scripts & Homebrew
+  source "$CWD"/home/install.sh
 
-# Install applications (depends on Homebrew)
-source "$CWD"/apps/install.sh
+  # Install applications (depends on Homebrew)
+  source "$CWD"/apps/install.sh
 
-# Install fonts to Font Book
-source "$CWD"/fonts/install.sh
+  # Install fonts to Font Book
+  source "$CWD"/fonts/install.sh
 
-# https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Soft&text=Go%20forth%20%26%20DEV!
-echo "                                                                                                         ,---.
- ,----.               ,---.                ,--.  ,--.          ,---.        ,------.  ,------.,--.   ,--.|   |
-'  .-./    ,---.     /  .-' ,---. ,--.--.,-'  '-.|  ,---.     |  o ,-.      |  .-.  \ |  .---' \  \`.'  / |  .'
-|  | .---.| .-. |    |  \`-,| .-. ||  .--''-.  .-'|  .-.  |    .'     /_     |  |  \  :|  \`--,   \     /  |  |
-'  '--'  |' '-' '    |  .-'' '-' '|  |     |  |  |  | |  |    |  o  .__)    |  '--'  /|  \`---.   \   /   \`--'
- \`------'  \`---'     \`--'   \`---' \`--'     \`--'  \`--' \`--'     \`---'        \`-------' \`------'    \`-'    .--.
-                                                                                                         '--'  "
+  # https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Soft&text=Go%20forth%20%26%20DEV!
+  echo "                                                                                                         ,---.
+   ,----.               ,---.                ,--.  ,--.          ,---.        ,------.  ,------.,--.   ,--.|   |
+  '  .-./    ,---.     /  .-' ,---. ,--.--.,-'  '-.|  ,---.     |  o ,-.      |  .-.  \ |  .---' \  \`.'  / |  .'
+  |  | .---.| .-. |    |  \`-,| .-. ||  .--''-.  .-'|  .-.  |    .'     /_     |  |  \  :|  \`--,   \     /  |  |
+  '  '--'  |' '-' '    |  .-'' '-' '|  |     |  |  |  | |  |    |  o  .__)    |  '--'  /|  \`---.   \   /   \`--'
+   \`------'  \`---'     \`--'   \`---' \`--'     \`--'  \`--' \`--'     \`---'        \`-------' \`------'    \`-'    .--.
+                                                                                                           '--'  "
 
-# if INSTALL_OH_MY_ZSH is true, then the .zshrc file has been updated
-if [ "$INSTALL_OH_MY_ZSH" = true ]; then
-  echo "ZSH. One last thing! You need to run now is 'source ~/.zshrc' to load your new .zshrc file, then you're good to go!"
-fi
+  # if INSTALL_OH_MY_ZSH is true, then the .zshrc file has been updated
+  if [ "$INSTALL_OH_MY_ZSH" = true ]; then
+    echo "ZSH. One last thing! You need to run now is 'source ~/.zshrc' to load your new .zshrc file, then you're good to go!"
+  fi
+}
+
+odq
